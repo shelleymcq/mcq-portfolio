8b34091108d12da8b6bb544e16167bab000c47a4
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2023-03-14 11:14:29 -0400

    landing page

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,12 +7,12 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className='w-full bg-cyan-900 fixed top-0 left-0 right-0 z-10'>
-        <div className='bg-cyan-900 justify-between p-5 mx-auto md:items-center md:flex'>
+      <nav className='w-full bg-slate-900 fixed top-0 left-0 right-0 z-10'>
+        <div className='justify-between p-5 mx-auto md:items-center md:flex'>
           <div>
             <div className='flex items center justify-between md:block'>
               <Link href='/'>
-                <div className='font-mono text-m text-cyan-500 hover:bg-cyan-800 rounded-sm px-2'>shelleymcq</div>
+                <div className='font-mono text-xl text-cyan-500 hover:bg-slate-800 rounded-sm px-2'>shelleymcq</div>
               </Link>
               <div className='md:hidden cursor-pointer text-slate-200' onClick={() => setNavbar(!navbar)}>
                 {navbar ? (
@@ -27,16 +27,16 @@ const Navbar = () => {
           <div>
             <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'p-12 md:p-0 block' : 'hidden'}`}>
               <ul className='h-screen items-center justify-center text-slate-200 md:h-auto md:flex'>
-                <li className='px-2 hover:bg-cyan-800 rounded-sm'>
+                <li className='px-2 hover:bg-slate-800 rounded-sm'>
                   <Link href='#homepage' onClick={() => setNavbar(!navbar)}>Home</Link>
                 </li>
-                <li className='px-2 hover:bg-cyan-800 rounded-sm'>
+                <li className='px-2 hover:bg-slate-800 rounded-sm'>
                   <Link href='#projects' onClick={() => setNavbar(!navbar)}>Projects</Link>
                 </li>
-                <li className='px-2 hover:bg-cyan-800 rounded-sm'>
+                <li className='px-2 hover:bg-slate-800 rounded-sm'>
                   <Link href='#about' onClick={() => setNavbar(!navbar)}>About</Link>
                 </li>
-                <li className='px-2 hover:bg-cyan-800 rounded-sm'>
+                <li className='px-2 hover:bg-slate-800 rounded-sm'>
                   <Link href='#contact' onClick={() => setNavbar(!navbar)}>Contact</Link>
                 </li>
               </ul>
--- pages/homepage.js
+++ pages/homepage.js
@@ -1,10 +1,37 @@
+import { FaGithub, FaLinkedin, FaRegEnvelope, FaReact } from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
+import atom from '../public/assets/atom.png'
+
 const Homepage = () => {
-  return (
-    <div>        
-      <div  className='bg-cyan-900 flex h-screen'>
-        <h1 className='m-auto text-slate-300 text-2xl'>Hero/Home Page</h1>
+  return (      
+    <div className='min-h-screen bg-slate-900 z-10 py-10'>
+        <div className='flex flex-col items-center justify-between px-10 md:flex-row'>
+          <div>
+            <div className='p-10'>
+              <h2 className='text-4xl md:text-6xl text-rose-700 py-2 font-semibold'>Shelley McHardy</h2>
+              <h3 className='text-xl md:text-2xl text-slate-500 py-2 font-medium'>software developer & educator</h3>
+              <p className='text-sm md:text-lg py-2 md:py-4 text-slate-300 leading-4'>Maybe add a little blurb. Maybe not.</p>
+            </div>
+            <div className='text-2xl py-1 md:py-5 px-10 flex'>
+              <ul className='flex space-x-4 items-center text-cyan-700'>
+                <li>
+                  <Link href='https://github.com/shelleymcq' target='_blank' rel='noopener noreferrer'><FaGithub /></Link>
+                </li>
+                <li>
+                  <Link href='https://www.linkedin.com/in/shelleymchardy/' target='_blank' rel='noopener noreferrer'><FaLinkedin /></Link>
+                </li>
+                <li>
+                  <Link href='mailto:shelleymcq.dev@gmail.com'><FaRegEnvelope /></Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+            <div className='px-10 md:px-3 lg:px-6'>
+              <Image src={atom} height={600} width={600} alt='stylized atom' />
+            </div>
+        </div>
       </div>
-  </div>
   )
 }
 
