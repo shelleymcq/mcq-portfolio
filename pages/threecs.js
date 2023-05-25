7955b9ebe3fd5facb032a82ebe69feedbe443ac1
Shelley McHardy <shelleymcq.dev@gmail.com>
2023-05-25 12:52:49 -0400

    added dad jokes and updated css

--- pages/threecs.js
+++ pages/threecs.js
@@ -14,20 +14,45 @@ const ThreeCs = () => {
       <section className="bg-slate-400 flex flex-col ">
         <h2 className="pt-20 mx-auto text-slate-700 text-3xl">coding</h2>
         <div className="flex justify-evenly flex-wrap p-4 rounded-xl shadow-2xl m-10">
-          <div className="bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
+          <div className="bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Learning Vue</h3>
+            <p className="p-4 text-slate-900 text-md">
+              I met a very passionate Vue developer at a networking event in May
+              and decided to try out a couple tutorials. While I have no plans
+              to switch from React at the moment, I feel like Vue might be a lot
+              more intuitive to learn.
+            </p>
+            <p className="p-4 text-slate-900 text-md">
+              Check out the silly thing I made if you need a laugh.
+            </p>
+            <div className="flex justify-around pt-1 text-sm">
+              <Link
+                href="https://shelleymcq.github.io/dad-jokes-vue/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="p-2 px-3 bg-cyan-800 text-slate-200 rounded-sm hover:bg-cyan-600"
+              >
+                Dad Jokes
+              </Link>
+            </div>
+            <p className="px-4 py-10 text-slate-900 text-sm">
+              Tech: Vue 3, icanhazdadjoke API
+            </p>
+          </div>
+          <div className="bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
             <p className="p-4 text-slate-900 text-sm">
               In April I decided to step outside my comfort zone and join a
-              coding competition. I am currently &apos;battling&apos; in
-              Lighthall&apos;s first Software Engineering Super League.
+              coding competition. I &apos;battled&apos; in Lighthall&apos;s
+              first Software Engineering Super League.
             </p>
             <div className="flex justify-around">
               <Image src={badge} alt="Level 4 SESL badge" />
             </div>
 
             <p className="p-4 text-slate-900 text-sm">
-              We are assigned to work in random teams and have from Monday to
+              We were assigned to work in random teams and given from Monday to
               Friday each week to complete increasingly more difficult
-              challenges. My teammates are in different time zones from New
+              challenges. My teammates were in different time zones from New
               Mexico to Nigeria, taking final exams, and one even got married
               between weeks 2 and 3!
             </p>
@@ -39,8 +64,8 @@ const ThreeCs = () => {
             </p>
           </div>
 
-          <div className="bg-slate-100 border-4 border-rose-900  rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>SESL Challenges</h3>
+          <div className="bg-slate-100 border-4 border-rose-900  rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">SESL Challenges</h3>
             <p className="p-4 text-slate-900 text-xs">
               Week 4: Restuarant choosing app, modeled very loosely on
               Between-both users need an account. Tech: React, Django
@@ -88,7 +113,7 @@ const ThreeCs = () => {
               counter that stored and displayed all clicks. Tech: React,
               firebase
             </p>
-            <div className="flex justify-around text-xs">
+            <div className="flex justify-around text-xs pb-2">
               <Link
                 href="https://www.shelleymcq.dev/clicks"
                 target="_blank"
@@ -105,9 +130,9 @@ const ThreeCs = () => {
       <section id="cooking" className="bg-slate-400 flex flex-col">
         <h2 className="pt-10 mx-auto text-slate-700 text-3xl">cooking</h2>
         <div className="flex justify-evenly flex-wrap p-4 rounded-xl shadow-2xl m-10">
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Jane&apos;s Mini Mocha Charlottes</h3>
-            <div className="flex text-xs">
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Jane&apos;s Mini Mocha Charlottes</h3>
+            <div className="pl-2 flex text-xs">
               <Link
                 href="https://www.bakewithalegend.com/onlineclasses"
                 target="_blank"
@@ -121,16 +146,16 @@ const ThreeCs = () => {
               This took FIVE hours from starting prep to clean up, but it was
               worth it!
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image
                 src={charlotte}
                 alt="chocolate cake with cream cheese swirl topping"
               />
             </div>
           </div>
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Dan&apos;s Challah</h3>
-            <div className="flex text-xs">
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Dan&apos;s Challah</h3>
+            <div className="pl-2 flex text-xs">
               <Link
                 href="https://www.bakewithalegend.com/onlineclasses"
                 target="_blank"
@@ -144,17 +169,17 @@ const ThreeCs = () => {
               I cannot believe I made this amazing bread! But since I took the
               picture, it must be true.
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image src={challah} alt="fresh baked braided and seeded bread" />
             </div>
           </div>
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Samosas</h3>
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Samosas</h3>
             <p className="p-4 text-slate-900 text-md">
               I made samosas for the first time last night. They&apos;re not
               pretty but they were DELICIOUS.
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image
                 src={samosas}
                 alt="freshly baked samosas on cookie sheet"
@@ -167,33 +192,34 @@ const ThreeCs = () => {
       <section id="crochet" className="bg-slate-400 flex flex-col ">
         <h2 className="pt-10 mx-auto text-slate-700 text-3xl">crochet</h2>
         <div className="flex justify-evenly flex-wrap p-4 rounded-xl shadow-2xl m-10">
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Eliza&apos;s Triceratops</h3>
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Eliza&apos;s Triceratops</h3>
             <p className="p-4 text-slate-900 text-lg">
               My neice loves dinosaurs and I love adorable amigurumi.
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image src={tango} alt="crochet purple triceratops" />
             </div>
           </div>
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Temperature Blanket</h3>
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Temperature Blanket</h3>
             <p className="p-4 text-slate-900 text-lg">
               PROGRESS PHOTO: I am super excited about this blanket, but I
               can&apos;t share any details because the person I&apos;m making it
               for doesn&apos;t know yet.
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image src={blanket} alt="crochet temperature blanket" />
             </div>
           </div>
-          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/3 md:p-5 lg:w-1/4">
-            <h3>Squeakoid!</h3>
+          <div className="flex flex-col bg-slate-100 border-4 border-rose-900 rounded-xl m-2 md:w-1/2 md:p-5 lg:w-1/3">
+            <h3 className="p-2 font-bold">Squeakoid!</h3>
             <p className="p-4 text-slate-900 text-lg">
-              Animal Crossing kept me grounded in 2020 and I still love
-              everything about it, especially the gyroids!
+              Animal Crossing kept me happy and socializing with friends
+              virtually in 2020 and I still love everything about it, especially
+              the gyroids!
             </p>
-            <div className="pt-3 place-self-center">
+            <div className="pt-3 pb-2 place-self-center">
               <Image src={gyroid} alt="crochet gyroid" />
             </div>
           </div>
