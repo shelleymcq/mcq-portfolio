e78d2971474c2dc007afcf1d612b2f6f3725caa5
Shelley McHardy <shelleymcq.dev@gmail.com>
2024-06-24 10:46:02 -0400

    vercel is mad?

--- pages/bbt.js
+++ pages/bbt.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TextField, Box, Stack, Button } from "@mui/material";
 import Image from "next/image";
 
